c81d494a805c99b01c34cc2a8a40c9cfbbe99f54
Alec O’Connor <Alec@AlecOConnor.com>
2017-10-10 22:10:42 -0500

    Initial Commit

---
+++ AOModalStatus/AOModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  AOModalStatus.h
+//  AOModalStatus
+//
+//  Created by Alec O'Connor on 10/10/17.
+//  Copyright © 2017 Alec O'Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AOModalStatus.
+FOUNDATION_EXPORT double AOModalStatusVersionNumber;
+
+//! Project version string for AOModalStatus.
+FOUNDATION_EXPORT const unsigned char AOModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOModalStatus/PublicHeader.h>
+
+
